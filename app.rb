cd87e54938ee7f59843d678af00843cf255e33ed
Dylan Sarikas <97060448+dylansarikas@users.noreply.github.com>
2024-05-15 20:22:08 +0000

    adds route for rock and rock erb

--- app.rb
+++ app.rb
@@ -4,3 +4,35 @@ require "sinatra/reloader"
 get("/") do
   erb(:rules)
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer = moves.sample
+
+  if @computer == "paper"
+    @output = "lost"
+  elsif @computer == "scissors"
+    @output = "won"
+  else
+    @output = "tied"
+  end
+
+  erb(:rock)
+end
+
+get ("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer = moves.sample
+
+  if @computer == "paper"
+    @output = "tied"
+  elsif @computer == "scissors"
+    @output = "lost"
+  else
+    @output = "won"
+  end
+
+  erb(:paper)
+end
